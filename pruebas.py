3bc9d7767295e5deca5f41c37073d8628985ef93
mirandaalex <amiranda.5_1997@hotmail.com>
2019-10-27 20:39:41 -0600

    Pruebas py

---
+++ pruebas.py
@@ -0,0 +1,46 @@
+from json import load
+from os import scandir, getcwd,listdir
+import importlib
+y=[]
+
+
+module=[]
+#SE IMPORTAN MODULOS DENTRO DE LIBS
+import importlib
+
+
+
+
+#ENCONTRAR JSON
+def ls(ruta = getcwd()):
+	   return [arch.name for arch in scandir() if arch.is_file()]
+
+def buscaHerramientas():
+	temp=ls()
+	for x in temp:
+		
+		if x[len(x)-4:]=="json":
+			y.append(x)
+			print(x,"Es igual y funciona")
+buscaHerramientas()
+
+
+data_file=open(y[0],"r") 
+data=load(data_file)
+
+
+
+tree = listdir(".\\libs\\")
+print(tree)
+for i in tree:
+	if i != "__init__.py" and i !="__pycache__":
+		name = i.replace(".py", "")
+		print(name)
+		name="libs."+name
+		module=importlib.import_module(name)
+		
+		class_ = getattr(module, data["Clase"])
+		instance = class_()
+		method_to_call = getattr(instance, data["Metodo"])
+		method_to_call()
+		
